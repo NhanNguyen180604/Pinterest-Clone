36aed7d9d1ae66102ca6a56834e8551ba43b47ad
22127228 <lglac22@clc.fitus.edu.vn>
2025-03-22 22:09:26 +0700

    Add account management UI

---
+++ app/src/main/java/com/example/pinterest_clone_test2/ui/settings/account_management.java
@@ -0,0 +1,110 @@
+package com.example.pinterest_clone_test2.ui.settings;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.example.pinterest_clone_test2.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link account_management#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class account_management extends Fragment {
+
+//    // TODO: Rename parameter arguments, choose names that match
+//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+//
+//    // TODO: Rename and change types of parameters
+//    private String mParam1;
+//    private String mParam2;
+
+    public account_management() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment account_management.
+     */
+    // TODO: Rename and change types and number of parameters
+//    public static account_management newInstance(String param1, String param2) {
+//        account_management fragment = new account_management();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+//    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account_management, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ConstraintLayout btnPersonalInformation = view.findViewById(R.id.btn_personal_information);
+        ConstraintLayout btnEmail = view.findViewById(R.id.btn_email);
+        ConstraintLayout btnPassword = view.findViewById(R.id.btn_password);
+        ImageButton btnBack = view.findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                NavController navController = Navigation.findNavController(view);
+                navController.navigateUp();
+            }
+        });
+        btnPersonalInformation.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                NavController navController = Navigation.findNavController(view);
+                navController.navigate(R.id.action_account_management_to_personal_information);
+            }
+        });
+        btnEmail.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                NavController navController = Navigation.findNavController(view);
+                navController.navigate(R.id.action_account_management_to_email);
+            }
+        });
+        btnPassword.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                NavController navController = Navigation.findNavController(view);
+                navController.navigate(R.id.action_account_management_to_password);
+            }
+        });
+    }
+}
