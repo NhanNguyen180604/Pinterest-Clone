432be98d4ef263edccd48485395ee8ad928f9563
Lê Trường <truongle.25082004@gmail.com>
2025-03-27 21:11:11 +0700

    Update User.java

--- app/src/main/java/com/example/pinterest_clone_test2/models/User.java
+++ app/src/main/java/com/example/pinterest_clone_test2/models/User.java
@@ -31,12 +31,12 @@ public class User {
     private Role role;
 
     // Danh sách mock users
-    private static List<User> mockUsers = new ArrayList<>(Arrays.asList(
+    public static List<User> mockUsers = new ArrayList<>(Arrays.asList(
             new User("password1", "user1@example.com", "Tân", "01/01/1990", Gender.Nam),
             new User("password2", "user2@example.com", "Tandy", "02/02/1992", Gender.Nữ),
             new User("password3", "user3@example.com", "Võ", "03/03/1995", Gender.Khác)
     ));
-    private static Map<String, String> tokenMap = new HashMap<>();
+    public static Map<String, String> tokenMap = new HashMap<>();
 
     // Constructor
     public User() {
