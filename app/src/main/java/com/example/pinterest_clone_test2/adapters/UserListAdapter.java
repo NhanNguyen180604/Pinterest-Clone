9b6b255f8b70107769bbcec3ed48916f256c1b7b
Lê Trường <truongle.25082004@gmail.com>
2025-03-28 14:36:06 +0700

    Update UserListAdapter.java

--- app/src/main/java/com/example/pinterest_clone_test2/adapters/UserListAdapter.java
+++ app/src/main/java/com/example/pinterest_clone_test2/adapters/UserListAdapter.java
@@ -1,33 +1,43 @@
 package com.example.pinterest_clone_test2.adapters;
 
+import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.pinterest_clone_test2.R;
 import com.example.pinterest_clone_test2.models.User;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.UserViewHolder> {
-    private List<User> userList;
 
-    public UserListAdapter(List<User> userList) {
-        this.userList = userList;
+    private List<User> userList = new ArrayList<>();
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void setUserList(List<User> newUsers) {
+        this.userList.clear();
+        this.userList.addAll(newUsers);
+        notifyDataSetChanged();
     }
 
     @NonNull
     @Override
     public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user, parent, false);
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_user, parent, false);
         return new UserViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
         User user = userList.get(position);
-        holder.tvUserName.setText(user.getName());
+        holder.tvUserName.setText(user.getFirstName());
         holder.tvUserEmail.setText(user.getEmail());
     }
 
