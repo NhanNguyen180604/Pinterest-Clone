d7fe605298266ae25c9bf7801a063c898dee2a2e
voduytan1 <voduytanbt1@gmail.com>
2025-04-15 01:49:14 +0700

    Add UserProfileViewModel to store user's profile state and info

--- app/src/main/java/com/example/pinterest_clone_test2/ui/user/UserProfileFragment.java
+++ app/src/main/java/com/example/pinterest_clone_test2/ui/user/UserProfileFragment.java
@@ -1,7 +1,6 @@
 package com.example.pinterest_clone_test2.ui.user;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
