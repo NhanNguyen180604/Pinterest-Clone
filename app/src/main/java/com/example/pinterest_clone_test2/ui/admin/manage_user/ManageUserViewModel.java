51e0b1f33ab296c20c7b780b45f0b53fd2a92118
Lê Trường <truongle.25082004@gmail.com>
2025-04-15 16:35:26 +0700

    Create ManageUserViewModel.java

---
+++ app/src/main/java/com/example/pinterest_clone_test2/ui/admin/manage_user/ManageUserViewModel.java
@@ -0,0 +1,15 @@
+package com.example.pinterest_clone_test2.ui.admin.manage_user;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.pinterest_clone_test2.models.User;
+
+public class ManageUserViewModel extends ViewModel {
+
+    
+}
