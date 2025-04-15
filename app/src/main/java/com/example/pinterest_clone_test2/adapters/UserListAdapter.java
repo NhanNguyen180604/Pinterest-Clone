c8a3e12fb606ad89e0406edf70c07a541595ff58
Lê Trường <truongle.25082004@gmail.com>
2025-04-15 16:33:12 +0700

    Create UserListAdapter.java

---
+++ app/src/main/java/com/example/pinterest_clone_test2/adapters/UserListAdapter.java
@@ -0,0 +1,20 @@
+package com.example.pinterest_clone_test2.adapters;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.pinterest_clone_test2.R;
+import com.example.pinterest_clone_test2.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.UserViewHolder> {
+
+
+}
