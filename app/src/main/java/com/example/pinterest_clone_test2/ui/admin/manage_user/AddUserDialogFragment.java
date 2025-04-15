d8dcf139c272bfc73e76063a8bd47c1a60e8d0f7
Lê Trường <truongle.25082004@gmail.com>
2025-04-15 16:34:55 +0700

    Create AddUserDialogFragment.java

---
+++ app/src/main/java/com/example/pinterest_clone_test2/ui/admin/manage_user/AddUserDialogFragment.java
@@ -0,0 +1,18 @@
+package com.example.pinterest_clone_test2.ui.admin.manage_user;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+import com.example.pinterest_clone_test2.R;
+import com.example.pinterest_clone_test2.models.User;
+
+public class AddUserDialogFragment extends DialogFragment {
+
+    
+}
