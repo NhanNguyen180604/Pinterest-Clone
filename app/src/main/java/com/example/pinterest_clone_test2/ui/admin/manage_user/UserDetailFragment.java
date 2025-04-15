e0f0951adf9535832756d0f0dbd425ad6b75de36
Lê Trường <truongle.25082004@gmail.com>
2025-04-15 16:35:52 +0700

    Update UserDetailFragment.java

--- app/src/main/java/com/example/pinterest_clone_test2/ui/admin/manage_user/UserDetailFragment.java
+++ app/src/main/java/com/example/pinterest_clone_test2/ui/admin/manage_user/UserDetailFragment.java
@@ -14,5 +14,29 @@ import com.example.pinterest_clone_test2.models.User;
 import java.io.Serializable;
 
 public class UserDetailFragment extends Fragment {
-    
+    private static final String ARG_USER = "user_data";
+
+    public static UserDetailFragment newInstance(User user) {
+        var fragment = new UserDetailFragment();
+        var args = new Bundle();
+        args.putSerializable(ARG_USER, (Serializable) user);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_user_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        assert getArguments() != null;
+        User user = (User) getArguments().getSerializable(ARG_USER);
+        assert user != null;
+        ((TextView) view.findViewById(R.id.text_name)).setText(user.getFirstName());
+        ((TextView) view.findViewById(R.id.text_email)).setText(user.getEmail());
+        // Thêm các info khác nếu cần
+    }
 }
