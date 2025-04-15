d29780fb8697b12aebcb10f73a0389641db96196
Lê Trường <truongle.25082004@gmail.com>
2025-04-15 16:35:46 +0700

    Create UserDetailFragment.java

---
+++ app/src/main/java/com/example/pinterest_clone_test2/ui/admin/manage_user/UserDetailFragment.java
@@ -0,0 +1,18 @@
+package com.example.pinterest_clone_test2.ui.admin.manage_user;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.example.pinterest_clone_test2.R;
+import com.example.pinterest_clone_test2.models.User;
+
+import java.io.Serializable;
+
+public class UserDetailFragment extends Fragment {
+    
+}
